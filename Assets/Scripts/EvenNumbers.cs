c65b016c18320df1551ba776781280855c238bf8
Glassnose <rollinsasia@yahoo.com>
2023-03-08 22:49:56 -0800

    Initial commit

---
+++ Assets/Scripts/EvenNumbers.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvenNumbers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sum = 22;
+        for (int count = 100; count >= 2; count--)
+            sum += count
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
